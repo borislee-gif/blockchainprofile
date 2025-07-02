b562735c7a8db6a2c2bff1bc8f037c06f38968a0
boris-gif <borislee0622@gmail.com>
2025-07-02 18:11:07 +0900

    first commit

---
+++ src/components/sections/Footer.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+import { Bio } from "../../data/constants";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+const FooterContainer = styled.div`
+  width: 100%;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+`;
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  align-items: center;
+  padding: 1rem;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Logo = styled.div`
+  font-weight: 600;
+  font-size: 20px;
+  color: ${({ theme }) => theme.primary};
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+    text-align: center;
+    font-size: 12px;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+const SocialMediaIcon = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Copyright = styled.p`
+  margin-top: 1.5rem;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.soft2};
+  text-align: center;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <Logo>Boris Anderson</Logo>
+        <Nav>
+          <NavLink href="#About">About</NavLink>
+          <NavLink href="#Skills">Skills</NavLink>
+          <NavLink href="#Projects">Projects</NavLink>
+        </Nav>
+        <SocialMediaIcons>
+          <SocialMediaIcon href={Bio.facebook} target="display">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 496 512" height="22" width="22" xmlns="http://www.w3.org/2000/svg" class="gb-blur-svg"><path d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z"></path></svg>
+          </SocialMediaIcon>
+          <SocialMediaIcon href={Bio.twitter} target="display">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="22" width="22" xmlns="http://www.w3.org/2000/svg" class="gb-blur-svg"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path></svg>
+          </SocialMediaIcon>
+          <SocialMediaIcon href={Bio.linkedin} target="display">
+            
+          </SocialMediaIcon>
+          <SocialMediaIcon href={Bio.insta} target="display">
+            <svg class="M" focusable="false" aria-hidden="true" viewBox="0 0 512 512" data-testid="MailOutlineRoundedIcon"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m-1 14H5c-.55 0-1-.45-1-1V8l6.94 4.34c.65.41 1.47.41 2.12 0L20 8v9c0 .55-.45 1-1 1m-7-7L4 6h16z"></path></svg>
+          </SocialMediaIcon>
+        </SocialMediaIcons>
+        <Copyright>&copy; 2025 Boris Anderson. All rights reserved.</Copyright>
+      </FooterWrapper>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/data/constants.js
@@ -0,0 +1,478 @@
+export const Bio = {
+  name: "Boris Anderson",
+  roles: [
+    "Full Stack Developer",
+    "Blockchain Developer"
+  ],
+  description:
+    "A passionate Blockchain Full-stack with expertise in Web3 and responsive web applications. Focusing on Smart contracts, trading dApps and Blockchain AI Agent on Solana, EVM based blockchain. Experience spans NFTs, crypto games, DeFi, DEXs and crypto trading bots. As a Full-Stack Engineer, I builds scalable applications using React, Next.js, TypeScript, Node.js, Rust, Golang, and Solidity. Passionate about blockchain innovation, he is always exploring new opportunities for growth.",
+  github: "https://github.com/borislee-gif",
+  resume:
+    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  twitter: "https://twitter.com/borisanderson",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://t.me/@da1asin",
+};
+
+export const skills = [
+  {
+    title: "BlockChain",
+    skills: [
+      {
+        name: "Smart Contract",
+        image:
+          "/images/symbols/smart contract.jpg",
+      },
+      {
+        name: "Web3",
+        image:
+          "/images/symbols/web3.png",
+      },
+      {
+        name: "Rust",
+        image:
+          "/images/symbols/rust.png",
+      },
+      {
+        name: "Anchor",
+        image:
+          "/images/symbols/anchor.jpg",
+      },
+      {
+        name: "Go",
+        image: "/images/symbols/go.png",
+      },
+      {
+        name: "Solidity",
+        image:
+          "/images/symbols/solidity.jpg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "/images/symbols/typescript.png",
+      },
+      {
+        name: "Node.js",
+        image:
+          "/images/symbols/nodejs.png",
+      },
+      {
+        name: "Django",
+        image:
+          "/images/symbols/django.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "PostgreSQL",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "ChromaDB",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "AI",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "LLM",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "React",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Next.js",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "TailwindCSS",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: " Git",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }
+    ],
+  }
+];
+
+export const projects = [
+  {
+    id: 7,
+    title: "Chainswap",
+    date: "Chainswap",
+    description:
+      "ChainSwap emerges as a revolutionary solution, bridging the gap between blockchains and empowering a more interconnected future. We leverage cutting-edge security protocols, built upon Chainlink's groundbreaking CCIP (Cross-Chain Interoperability Protocol), to ensure a secure layer 5 environment for your cross-chain transactions.",
+    image:
+      "/images/projects/chainswap.gif",
+    tags: ["Solana", "Solidity", "Bitcoin", "Node.js", "Next.js"],
+    category: "android app",
+    github: "",
+    webapp: "https://www.chainswap.tech",
+  },
+  {
+    id: 7,
+    title: "The WiseTrade",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Providing liquidity is often rewarded. This incentive allows providers to put their holdings to work while limiting risks. The act of providing liquidity is now widely spread and part of most portfolios. Especially with recent point reward systems that make incentives stronger.",
+    image:
+      "/images/projects/wisetrade.gif",
+    tags: ["Solana", "TypeScript", "Node.js", "Vue.js"],
+    category: "android app",
+    github: "",
+    webapp: "https://thewise.trade",
+  },
+  {
+    id: 7,
+    title: "DASHCOIN",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "This is official website of Dashcoin on solana ecosystem, I built the entire page using nextjs and typescript, in the backend it fetches the all web3 data using dune api",
+    image:
+      "/images/projects/dashcoin.webp",
+    tags: ["Solana", "TypeScript", "Node.js", "Next.js"],
+    category: "android app",
+    github: "",
+    webapp: "https://dashcoin.meme",
+  },
+  {
+    id: 7,
+    title: "AMANA Defi",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Amana is a next-generation decentralized finance (DeFi) yield aggregator designed to optimize and simplify the process of earning yield on your crypto assets. By leveraging advanced strategies and a multi-chain infrastructure, Amana ensures that your investments generate the highest possible returns, all while minimizing the complexity of managing assets across multiple DeFi platforms.",
+    image:
+      "/images/projects/amana.webp",
+    tags: ["Solana", "Solidity", "Node.js", "Typescript", "Next.js"],
+    category: "android app",
+    github: "https://amanadefi.com",
+    webapp: "https://amanadefi.com",
+  },
+  {
+    id: 7,
+    title: "Rugsdotfun Fork",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Forked the rugsdotfun frontend recently. Rugsdotfun is the fastest growing solana's trading casino platform",
+    image:
+      "/images/projects/rugsfun.webp",
+    tags: ["Next.js", "Solana"],
+    category: "android app",
+    github: "https://rugsfun-fork.vercel.app",
+    webapp: "https://rugsfun-fork.vercel.app",
+  },
+  {
+    id: 7,
+    title: "Memepad Launchpad",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "MEMEPad is where the REAL memecoin degens go to get filthy rich. Next-gen coins, rug-proof, and ready to PUMP. I built the smart contract of memepad using rust and also backend aswell as integration of memepad with Meteora Pool",
+    image:
+      "/images/projects/memepad.webp",
+    tags: ["Solana", "Node.js", "TypeScript", "Rust"],
+    category: "android app",
+    github: "https://memepad.ai",
+    webapp: "https://memepad.ai",
+  },
+  {
+    id: 7,
+    title: "Fork Trojanbot SOL",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Trojan Bot is a popular trading and sniping bot on the Solana blockchain that's designed to help traders execute trades with speed and efficiency.If you're developing a wallet tracking system like we discussed earlier, understanding how Trojan Bot and similar tools operate can help you identify how trades are executed on-chain and potentially improve your transaction parsing logic.",
+    image:
+      "/images/projects/trojan.webp",
+    tags: ["Telegram", "Node.js", "TypeScript", "Solana"],
+    category: "android app",
+    github: "https://t.me/trojanusrobot",
+    webapp: "https://t.me/trojanusrobot",
+  },
+  {
+    id: 7,
+    title: "Copytrading bot",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "The bot is monitoring trader's wallet. And execute trading according to the trader's wallet. For example, if the trader buys tokens, this bot buys the tokens too and if the trader sells token, the bot sells token too.",
+    image:
+      "/images/projects/copytrade.webp",
+    tags: ["Solana", "Typescript", "Node.js"],
+    category: "android app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 7,
+    title: "Meteora Volume Bot",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Automated SOL Distribution: Distributes SOL to new wallets. Endless Buy and Sell Swaps: Performs simultaneous buy and sell transactions. Swap with Jupiter V6: Swap is performed with Jupiter V6 swap aggregator. Configurable Parameters: Allows customization of buy amounts, intervals, distribution settings, and more.",
+    image:
+      "/images/projects/meteora.webp",
+    tags: ["Solana", "TypeScript", "Node.js"],
+    category: "android app",
+    github: "https://github.com/mooncitydev/solana-meteora-volume-bot",
+    webapp: "https://github.com/borisanderson/Quiz-Earn",
+  },
+  {
+    id: 7,
+    title: "Pumpfun Fork",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Pump.fun is an exciting concept allowing creators and degens to come together in perfect unison. New Token projects can be launched for absolute minimal costs (~0.02 SOL) and 0 initial liquidity. It allows communities to come together and grow, with the end goal of achieving 100% of the bonding curve and then liquidity raised is used to launch the project onto raydium",
+    image:
+      "/images/projects/pumpfun.gif",
+    tags: ["Solana", "TypeScript", "Node.js", "MongoDB", "Next.js"],
+    category: "android app",
+    github: "https://pumpfun-eta.vercel.app",
+    webapp: "https://pumpfun-eta.vercel.app",
+  },
+  {
+    id: 7,
+    title: "BNRY BNB Bridge",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "BinaryBit is not only enhances security and privacy in financial transactions but also aligns seamlessly with the decentralized and cutting-edge nature of the content creation space. This strategic integration of AI technology and a dedicated cryptocurrency creates a robust ecosystem, ensuring a streamlined and secure user experience while fostering a forward-thinking approach to online content generation.",
+    image:
+      "/images/projects/bnry.gif",
+    tags: ["Next.js", "TypeScript", "MongoDB", "Node.js"],
+    category: "android app",
+    github: "https://platform.binarybit.pro",
+    webapp: "https://platform.binarybit.pro",
+    url: ""
+  },
+  {
+    id: 112,
+    title: "SocialAI frontend development",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "SocialAI is a new Twitter clone where instead of humans, different types of AI bots respond to your posts. In this project, I built handsome pages using Next.js and TailwindCSS.",
+    image:
+      "/images/projects/socialai.gif",
+    tags: [
+      "Node.js",
+      "TypeScript",
+      "Next.js",
+      "TailWindCSS",
+    ],
+    category: "web app",
+    github: "https://github.com/borisanderson/DecisionHub",
+    webapp: "https://social-ai-fe.vercel.app",
+  },
+  {
+    id: 11,
+    title: "Lmao Fun",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "PumpFun Forking Project on Solana",
+    image:
+      "/images/projects/lmao.fun.png",
+    tags: [
+      "Web3",
+      "Solana",
+      "Smart Contract",
+      "Anchor",
+      "Rust",
+      "TypeScript",
+      "Dex",
+      "Node.js",
+      "MongoDB",
+      "Express",
+    ],
+    category: "web app",
+    github: "https://github.com/borisanderson/DecisionHub",
+    webapp: "https://lmao.fun/",
+  },
+  {
+    id: 111,
+    title: "Twitter Scan bot",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "I built a function to query and scrape any potential contract addresses from each new tweet from marked accounts Pipe those contract addresses to an enhanced buy function on either raydium/pumpfun/uniswap (with adjustable high gas fee's/jito) I started with just solana for pumpfun/raydium/meteora",
+    image:
+      "/images/projects/xscan.jpg",
+    tags: [
+      "Node.js",
+      "TypeScript",
+      "Solana Web3"
+    ],
+    category: "web app",
+    github: "https://github.com/borisanderson/DecisionHub",
+  },
+  {
+    id: 9,
+    title: "Crashr Marketplace",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Cardano NFT Marketplace",
+    image:
+      "/images/projects/crashr.5aaa50e2cb5c180b966a.png",
+    tags: [
+      "Web3",
+      "React",
+      "TypeScript",
+      "Cardano",
+      "AWS",
+      "Node.js"
+    ],
+    category: "web app",
+    github: "https://github.com/borisanderson/Trackify",
+    webapp: "https://www.crashr.io/",
+  },
+  {
+    id: 0,
+    title: "Solana Telegram Bot",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Solana Telegram Trading Bot(Trojan Bot Fork)",
+    image:
+      "/images/projects/solana-tg-bot.13fdcc5cc95acb581d4a.png",
+    tags: ["Web3", "Telegram Bot", "Node.js", "MongoDB", "Solana", "Trading Bot"],
+    category: "web app",
+    github: "",
+    webapp: "https://t.me/oracle_sol_trading_tg_bot",
+  },
+  {
+    id: 1,
+    title: "Solana Volume Bot",
+    date: "Oct 2022 - Jan 2023",
+    description:
+      "Solana Volume Bot",
+    image:
+      "/images/projects/volume bot.79132f922009c59fd33d.jpg",
+    tags: [
+      "Solana",
+      "Trading Bot",
+      "PumpFun",
+      "Raydium",
+      "Meteora",
+      "Pump Swap",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 2,
+    title: "PumpFun Bundler",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Solana PumpFun Bundler",
+    image:
+      "/images/projects/pumpfun-bundler.8091f7a86437adfc25ff.png",
+    tags: ["Web3", "PumpFun", "Solana", "Bundler", "Trading Bot"],
+    category: "machine learning",
+    github: "",
+    webapp: ""
+  },
+  {
+    id: 3,
+    title: "Sweep Slots",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "online casino-style games based on Roulette, Blackjack, Video Poker, Table Games and Big Jackpot Slots.",
+    image:
+      "/images/projects/sweepslots.f9506d610ee03f25857e.png",
+    tags: ["PHP", "MySQL", "Casino", "Game",],
+    category: "android app",
+    github: "",
+    webapp: "https://sweepslots.com/",
+  },
+  {
+    id: 10,
+    title: "Sleepsia",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Pillow SaaS project",
+    image:
+      "/images/projects/sleepsia.f3977f593b561b07cada.png",
+    tags: ["Shopify", "AWS"],
+    category: "android app",
+    github: "",
+    webapp: "https://www.sleepsia.com/",
+  },
+  {
+    id: 4,
+    title: "OXFX",
+    date: "Jul 2021",
+    description:
+      "Centralized Exchange",
+    image:
+      "/images/projects/oxfx.6ccf68eda6414792909e.png",
+    tags: ["Web3", "CEX", "TypeScript", "Node.js", "React", "Express", ".Net", "C#"],
+    category: "web app",
+    github: "",
+    webapp: "https://oxfx-fe.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "MD Token",
+    date: "Jun 2021",
+    description:
+      "MD Token Official Website. Raffle, Staking, Redeem, Governance",
+    image:
+      "/images/projects/mdtoken.0e3549ebbc347d4de2ae.png",
+    tags: ["Web3", "React", "TypeScript", "Styled Components", "Cardano", "web3"],
+    category: "web app",
+    github: " ",
+    webapp: "https://mdtokens.com/",
+  },
+  {
+    id: 6,
+    title: "Dexmarketcap App",
+    date: "Jun 2021",
+    description:
+      "DMC Next-Gen DEX Analytics, on-chain realtime data & Wallet tracker with PnL.",
+    image:
+      "/images/projects/DexmarketCap.d5d19efdd308c6c43234.png",
+    tags: ["Web3", "React", "TypeScript", "Styled Components", "Node.js", "Express", "EVM", "Scraping", "Indexing"],
+    category: "web app",
+    github: "",
+    webapp: "https://dexmarketcap.app/",
+  },
+  {
+    id: 7,
+    title: "QuantumFi",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      "Quantum Fi Landing Page",
+    image:
+      "/images/projects/quantumfi.9b177fec371589b05503.png",
+    tags: ["React", "TypeScript", "Styled Components"],
+    category: "android app",
+    github: "",
+    webapp: "https://quantum-fi-frontend.vercel.app/",
+  },
+];
+
